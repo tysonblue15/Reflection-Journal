7b645a273976b3ae364318188056948b0c5c9238
Tyson Thomas <tysonmthomas@gmail.com>
2024-01-07 20:50:02 -0500

    First Save Features With Minor Bugs

--- log.js
+++ log.js
@@ -3,17 +3,20 @@ const emotion = document.getElementById('emotion');
 const rating = document.getElementById('rating');
 const currentDate = document.getElementById('currentDate');
 
+const submit = document.getElementById('submitButton');
+
 //Needs to be saved
 const descriptionCollection = [];
 const emotionCollection = [];
 const ratingCollection = [];
 const currentDateCollection = [];
+const dataCollection = [];
 
-submitButton.addEventListener('click', () => {
+submit.addEventListener('click', () => {
     if(descriptionCollection.length >= 10){
         descriptionCollection.pop();
     }
-    descriptionCollection.unshift(description.textContent)
+    descriptionCollection.unshift(description.value)
     
     
     let emotionMap ={};
@@ -29,7 +32,7 @@ submitButton.addEventListener('click', () => {
     if(emotionCollection.length >= 10){
         emotionCollection.pop();
     }
-    emotionCollection.unshift(emotion.textContent);
+    emotionCollection.unshift(emotion.value);
 
     emotionCollection.forEach(element => {
         emotionMap[element]++;
@@ -52,4 +55,52 @@ submitButton.addEventListener('click', () => {
         currentDateCollection.pop();
     }
     currentDateCollection.unshift(currentDate.value)
+
+    let newObj = {
+        des: description.value ,
+        emo: emotion.value ,
+        rat: rating.value ,
+        dat: currentDate.value
+    };
+    localStorage.clear();
+    //ERROR
+    if(localStorage == null){
+        SetUp();
+    }
+    else{
+        SetUp();
+        //ObjectMover();
+    }
+
+    localStorage.setItem("obj1", newObj);
+    console.log("1: " + newObj['des']);
+
+    for (let i = 2; i <= 10; i++) {
+        let holderObj = JSON.parse(localStorage.getItem("obj" + i));
+        console.log("\n" + i + ": " + holderObj['des']);
+    }
 })
+
+function SetUp() {
+    console.log("Set Up\n");
+    let objects = [];
+    for (let i = 0; i < 10; i++) {
+        objects[i] = {
+            des: "Test",
+            emo: "",
+            rat: "",
+            dat: ""
+        };
+        localStorage.setItem("obj" + (i + 1), JSON.stringify(objects[i]));
+    }
+}
+
+function ObjectMover() {
+    let i = 1;
+    while(i < 10){
+        let currentObj = localStorage.getItem("obj"+i);
+        localStorage.setItem("obj"+i+1, currentObj);
+
+        i++;
+    }
+}
