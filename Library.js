57a9604db94f7ec9118ecd5788648de417240ee1
Tyson Thomas <tysonmthomas@gmail.com>
2024-01-12 22:22:00 -0500

    Data Transfer
    -All data now transfers and is displayed on the new screen

--- Library.js
+++ Library.js
@@ -7,6 +7,9 @@ newLog.addEventListener('click', () => {
 function updateText(){
     for (let i = 1; i <= 1; i++) {
         let holderObj = JSON.parse(localStorage.getItem("obj" + i));
-        document.getElementById('log'+i+'Description').innerText = JSON.stringify(holderObj['des']); //Use only holderObj['des'] to remove ""
+        document.getElementById('log'+i+'Description').innerText = "1: " + JSON.stringify(holderObj['des']); //Use only holderObj['des'] to remove ""
+        document.getElementById('date'+i).innerText = "Here"; //holderObj['dat']
+        document.getElementById('emotion'+i).innerText = holderObj['emo'];
+        document.getElementById('rating'+i).innerText = holderObj['rat'] + "/10";
     }    
 }
